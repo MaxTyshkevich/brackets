3e3367412e1183a19ad8b41a7262dafde8e7d3e4
Max Tyskevich <2842778@gmail.com>
2021-03-15 09:21:37 +0300

    16 from 20 :(

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let openBrackets = [];
+    let closeBrackets = [];
+
+    bracketsConfig.forEach(([open, close]) => {
+        openBrackets.push(open);
+        closeBrackets.push(close);
+    });
+
+    let result = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (result < 0) return false;
+
+        if (openBrackets.includes(str[i])) {
+            let count = openBrackets.indexOf(str[i]) + 1; // 1,2,3
+            result += count;
+        }
+        if (closeBrackets.includes(str[i])) {
+            let count = closeBrackets.indexOf(str[i]) + 1; // 1,2,3
+            result -= count;
+        }
+    }
+    return !result ? true : false;
+};
